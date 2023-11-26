bd5741c832f576f6853857863ca822a04496bf75
Peter Suchsland <visiondoctor2020@gmail.com>
2023-11-26 14:58:36 +0700

    Update set.lua
    autoread autoreload of files that change externally ie vscode

--- lua/petesuch/set.lua
+++ lua/petesuch/set.lua
@@ -18,6 +18,9 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
---vim.opt.isfname:append(@-@)
---vim.opt.undodir = os.getenv($HOME) .. "/.vim/undodir"
-
+-- Autoreload if file changes in external editor
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
