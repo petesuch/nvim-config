56061fdd519f14e6e588825ab6189c69d2944e56
Peter Suchsland <visiondoctor2020@gmail.com>
2025-02-26 12:46:59 +0700

    Update packer.lua

--- lua/petesuch/packer.lua
+++ lua/petesuch/packer.lua
@@ -1,49 +1,68 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+  -- Packer manages itself
   use 'wbthomason/packer.nvim'
 
+  -- OneDark Theme (Navarasu) with a black background
+  use {
+    'navarasu/onedark.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "darker", -- Available: dark, darker, cool, deep, warm, warmer
+        term_colors = true, -- Ensure terminal colors match
+        colors = { bg = "#000000" } -- Force pure black background
+      })
+      require("onedark").load()
+    end
+  }
+
+  -- Telescope for fuzzy finding
   use {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.2',
+    requires = { 'nvim-lua/plenary.nvim' }
   }
-    
-use 'navarasu/onedark.nvim'
-  require('onedark').setup {
-    style = 'darker',
-      transparent = false
+
+  -- Treesitter for syntax highlighting
+  use { 
+    'nvim-treesitter/nvim-treesitter', 
+    run = ":TSUpdate"
   }
-require('onedark').load()
-    
-  -- Lualine and nvim-tree need devicons
-  use { 'nvim-tree/nvim-web-devicons' }
-  use { 'nvim-lualine/lualine.nvim', }
-  use { 'nvim-tree/nvim-tree.lua', }
 
-  use { 'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate' }
+  -- Lualine: Status line with devicons
+  use { 
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons' }
+  }
 
+  -- Nvim-tree: File explorer
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = { 'nvim-tree/nvim-web-devicons' }
+  }
+
+  -- LSP Setup (Language Server Protocol)
   use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
     requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'L3MON4D3/LuaSnip'},
+      {'neovim/nvim-lspconfig'},  -- Base LSP configurations
+      {'williamboman/mason.nvim'},  -- LSP installer
+      {'williamboman/mason-lspconfig.nvim'},  -- Bridge for Mason & LSPConfig
+      {'hrsh7th/nvim-cmp'},  -- Autocompletion framework
+      {'hrsh7th/cmp-nvim-lsp'},  -- Completion source for LSP
+      {'L3MON4D3/LuaSnip'},  -- Snippet engine
     }
   }
 
-use {
+  -- Comment.nvim for easy commenting
+  use {
     'numToStr/Comment.nvim',
     config = function()
-        require('Comment').setup()
+      require('Comment').setup()
     end
-}
-
+  }
 end)
