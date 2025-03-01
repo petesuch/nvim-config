406935ba9b53aa85232fb36ae5e996fc4f7b5555
Peter Suchsland <visiondoctor2020@gmail.com>
2025-03-01 08:47:22 +0700

    removed remap merged with set

--- lua/petesuch/set.lua
+++ lua/petesuch/set.lua
@@ -1,17 +1,24 @@
--- Set leader key
-vim.g.mapleader = "  "
+-- set.lua: Neovim settings and key mappings
 
--- Cursor and numbering
+-- 🌟 Leader Key
+vim.g.mapleader = "  "  -- I like double spaces
+
+-- 🌟 UI Settings
 vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 3
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "85"
 
--- Indentation preferences
-vim.opt.expandtab = true      -- Use spaces instead of tabs
-vim.opt.shiftwidth = 2        -- Number of spaces for indentation
-vim.opt.tabstop = 2           -- Number of spaces per tab character
-vim.opt.softtabstop = 2       -- Number of spaces when pressing <Tab>
-vim.opt.smartindent = true    -- Enable smart indentation
+-- 🌟 Indentation
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.shiftwidth = 2     -- Number of spaces for indentation
+vim.opt.tabstop = 2        -- Number of spaces per tab character
+vim.opt.softtabstop = 2    -- Number of spaces when pressing <Tab>
+vim.opt.smartindent = true -- Enable smart indentation
 
 -- Python-specific indentation
 vim.api.nvim_create_autocmd("FileType", {
@@ -23,22 +30,23 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- UI settings
+-- 🌟 Other Editor Behavior
 vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 3
-vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "85"
-
--- Auto-reload files when changed externally
-vim.opt.autoread = true
+vim.opt.autoread = true  -- Auto-reload files if changed externally
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "FocusGained" }, {
   command = "checktime",
 })
+
+-- 🌟 Key Mappings
+-- Open NvimTree safely (if installed)
+if vim.fn.exists(":NvimTreeToggle") == 2 then
+  vim.keymap.set("n", "<leader>ex", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+else
+  print("Warning: NvimTree not installed!")
+end
 
