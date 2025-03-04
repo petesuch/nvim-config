d9ae80cbfa23972794dc4551c421cecc592d3645
Peter Suchsland <peter.suchsland@gmail.com>
2025-03-05 01:42:19 +0700

    removed lua indent

--- lua/petesuch/nvimtree.lua
+++ lua/petesuch/nvimtree.lua
@@ -1,16 +1,16 @@
 -- setup with some options
 require("nvim-tree").setup({
-    sort = {
-        sorter = "case_sensitive",
-    },
-    view = {
-        width = 30,
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = true,
-    },
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
 })
 
