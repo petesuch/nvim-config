e58bf5d46857b50a8efae4ae60809f76a67bed2e
Peter Suchsland <visiondoctor2020@gmail.com>
2025-03-09 01:00:42 +0700

    Update nvimtree.lua
    includes glyphs

--- lua/petesuch/nvimtree.lua
+++ lua/petesuch/nvimtree.lua
@@ -1,5 +1,5 @@
--- setup with some options
-require("nvim-tree").setup({
+----setup with some options
+require'nvim-tree'.setup {
   sort = {
     sorter = "case_sensitive",
   },
@@ -12,5 +12,32 @@ require("nvim-tree").setup({
   filters = {
     dotfiles = false,
   },
-})
+  renderer = {
+    icons = {
+      glyphs = {
+        default = '',
+        symlink = '',
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌"
+        },
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+      }
+    }
+  }
+}
 
