e705b8c5da7a04a6635133097abe36f7e02bb494
Peter Suchsland <visiondoctor2020@gmail.com>
2025-04-12 18:54:53 +0700

    Update set.lua

--- lua/petesuch/set.lua
+++ lua/petesuch/set.lua
@@ -56,4 +56,9 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "FocusGained" }, {
   command = "checktime",
 })
 
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
 
